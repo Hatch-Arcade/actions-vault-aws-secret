a5991ec4a6f7c19a9674be6e33dc572c8a2064d6
Khoa Mai <khoa.mai@hatch.live>
2020-10-27 10:58:45 +0200

    update set-env to fix CVE-2020-15228

--- aws_sts_secret.sh
+++ aws_sts_secret.sh
@@ -20,16 +20,17 @@ echo ::add-mask::"${AWS_ACCESS_KEY_ID}"
 echo ::add-mask::"${AWS_SESSION_TOKEN}"
 echo ::add-mask::"${AWS_SECRET_ACCESS_KEY}"
 
-echo ::set-env name=AWS_ACCESS_KEY_ID::"${AWS_ACCESS_KEY_ID}"
-echo ::set-env name=AWS_SESSION_TOKEN::"${AWS_SESSION_TOKEN}"
-echo ::set-env name=AWS_SECRET_ACCESS_KEY::"${AWS_SECRET_ACCESS_KEY}"
+echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> ${GITHUB_ENV}
+echo "AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}" >> ${GITHUB_ENV}
+echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> ${GITHUB_ENV}
+
 echo ::set-output name=aws_access_key_id::"${AWS_ACCESS_KEY_ID}"
 echo ::set-output name=aws_session_token::"${AWS_SESSION_TOKEN}"
 echo ::set-output name=aws_secret_access_key::"${AWS_SECRET_ACCESS_KEY}"
 
-echo ::set-env name=ASSUME_ROLE_ARN::"${ASSUME_ROLE_ARN}"
+echo "ASSUME_ROLE_ARN=${ASSUME_ROLE_ARN}" >> ${GITHUB_ENV}
 echo ::set-output name=assume_role_arn::"${ASSUME_ROLE_ARN}"
 
-echo ::set-env name=VAULT_AWS_SECRET_BACKEND_ROLE::"${VAULT_AWS_SECRET_BACKEND_ROLE}"
+echo "VAULT_AWS_SECRET_BACKEND_ROLE=${VAULT_AWS_SECRET_BACKEND_ROLE}" >> ${GITHUB_ENV}
 echo ::set-output name=vault_aws_secret_backend_role::"${VAULT_AWS_SECRET_BACKEND_ROLE}"
 
